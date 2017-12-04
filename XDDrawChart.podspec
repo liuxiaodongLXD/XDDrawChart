25dbdb84241606e0e2cef89f4eb99086cb29ae66
lxd <lxd2020nba@163.com>
2017-12-04 16:54:07 +0800

    示例Demo

---
+++ XDDrawChart.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+s.name         = 'XDDrawChart'
+s.version      = '0.0.1'
+s.summary      = '绘制柱状图、折线图'
+s.homepage     = 'https://github.com/liuxiaodongLXD/XDDrawChart'
+s.license      = 'MIT'
+s.authors      = {'liuxiaodongLXD' => 'lxd2020nba@163.com'}
+s.platform     = :ios, '8.0'
+s.source       = {:git => 'https://github.com/liuxiaodongLXD/XDDrawChart.git', :tag => s.version}
+s.source_files = "XDDrawChart/header/*.{h,m}"
+s.vendored_frameworks = 'XDDrawChart/XDDraw.framework'
+s.requires_arc = true
+end
---
+++ XDDrawChartDemo/XDDrawChartDemo/XDDrawChart/header/XDDraw.h
@@ -0,0 +1,21 @@
+//
+//  XDDraw.h
+//  XDDraw
+//
+//  Created by apple on 2017/12/4.
+//  Copyright © 2017年 lxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "XDDrawChatView.h"
+#import "XDDrawData.h"
+
+//! Project version number for XDDraw.
+FOUNDATION_EXPORT double XDDrawVersionNumber;
+
+//! Project version string for XDDraw.
+FOUNDATION_EXPORT const unsigned char XDDrawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XDDraw/PublicHeader.h>
+
+
